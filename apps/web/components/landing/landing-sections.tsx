058eb9ab06bfa16b0a82c45f7a7a7aef6127a436
Norton-Gauss <innovate@nortongauss.com>
2026-02-18 17:26:27 +0100

    Update landing-sections.tsx

--- apps/web/components/landing/landing-sections.tsx
+++ apps/web/components/landing/landing-sections.tsx
@@ -1,7 +1,12 @@
+'use client';
+
 import dynamic from "next/dynamic";
 
 const LandingSectionsContent = dynamic(
-  () => import("@/components/landing/landing-sections-content").then((mod) => mod.LandingSectionsContent),
+  () =>
+    import("@/components/landing/landing-sections-content").then(
+      (mod) => mod.LandingSectionsContent
+    ),
   { ssr: false }
 );
 
